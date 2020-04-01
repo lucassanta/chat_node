9ed891dba65947cfbebd1d9a3b5396623b3d7d53
Lucas Santana <lanatnas36@gmail.com>
2020-04-01 08:48:15 -0300

    auetenticação server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 
@@ -8,35 +7,57 @@ const io = require('socket.io')(server); //protocolo ws websocket
 
 const usuario = require("./usuario");
 
+var bcrypt = require('bcryptjs');
+
+var crypt = (senha) =>{ 
+    return new Promise(function(resolve, reject){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(senha, salt);
+            resolve(hash);
+        });
+}
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
-app.engine('html',require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/',(req,res) => {
+app.use('/', (req, res) => {
     res.render('index.html');
 });
 
 let messages = [];
 
 io.on('connection', socket => {
-    console.log("Socket conectado:"+socket.id);
-    socket.broadcast.emit("teste", "hi");
-    socket.on('login', user=>{   
+    console.log("Socket conectado:" + socket.id);
+    //socket.broadcast.emit("teste", "hi"); //teste 
+    // console.log(usuario.update_user(4, "Fred", "123333")); //teste
+    socket.on('login', user => {
         console.log(user);
-        usuario(user.username, user.password).then(function(result){
-            if(result.length != 0)
-            socket.emit("logged", result);  
-         }).catch(function(err){
-             console.log(err);
-         });
-            
+        usuario.select_user(user.username).then(function (result) {
+                if (result.length != 0) {
+                    if(bcrypt.compareSync(user.password, result[0].senha)) socket.emit("logged", result);
+                }
+                else socket.emit("loginError", "Usuário e/ou senha incorretos!");
+        }).catch(function (err) {
+            console.log(err);
+        });
+
     });
 
-    socket.on('sendMessage',data =>{
+    socket.on('register',register =>{
+        crypt(register.senha).then(function(senha){
+            senhaHash = senha;
+            usuario.insert_user(register.nome, register.email,senhaHash,register.telefone);
+        }).catch(function(){
+            console.log("Não foi inserido!");
+        });
+
+    });
+
+    socket.on('sendMessage', data => {
         console.log(data);
         messages.push(data);
-        socket.broadcast.emit('receivedMessage', data); 
+        socket.broadcast.emit('receivedMessage', data);
     });
 });
 server.listen(3000, () => {
